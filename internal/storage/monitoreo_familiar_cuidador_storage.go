e53c9c5d37464262f598d16ea002515506e37c9f
Michael Bird Medina Reyes <mike97medi@gmail.com>
2026-06-11 14:33:45 -0500

    feat: agregar metodo crear relacion

--- internal/storage/monitoreo_familiar_cuidador_storage.go
+++ internal/storage/monitoreo_familiar_cuidador_storage.go
@@ -3,3 +3,7 @@ package storage
 import "github.com/xxmela-anchundiaxx/Proyecto-MediCare-Adulto-Mayor/internal/models"
 
 var Relaciones []models.CuidadorPaciente
+
+func CrearRelacion(relacion models.CuidadorPaciente) {
+	Relaciones = append(Relaciones, relacion)
+}
