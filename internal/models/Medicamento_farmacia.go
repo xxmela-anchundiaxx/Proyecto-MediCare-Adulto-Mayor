1d162fcad381df034228ad0cee41a78b23aa019c
xxmela-anchundiaxx <melanieanchundia27@gmail.com>
2026-06-11 17:56:03 -0500

    Agregar datos en json

---
+++ internal/models/Medicamento_farmacia.go
@@ -0,0 +1,10 @@
+package models
+
+type Medicamento_farmacia struct {
+	ID         int     `json:"id"`
+	Nombre     string  `json:"nombre"`
+	Precio     float64 `json:"precio"`
+	Disponible bool    `json:"disponible"`
+	Farmacia   string  `json:"farmacia"`
+	Horario    string  `json:"horario"`
+}
