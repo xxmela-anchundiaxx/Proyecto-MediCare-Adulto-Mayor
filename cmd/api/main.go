589bc3d3226ead15799284c5ea102d72e7d3e179
xxmela-anchundiaxx <melanieanchundia27@gmail.com>
2026-06-11 17:58:25 -0500

    se creo el main

---
+++ cmd/api/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"proyectodemedicare/internal/handlers"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	r.Post("/api/v1/farmacia", handlers.CreateMedicamento)
+	r.Get("/api/v1/farmacia", handlers.GetMedicamentos)
+	r.Get("/api/v1/farmacia/{id}", handlers.GetMedicamento)
+	r.Put("/api/v1/farmacia/{id}", handlers.UpdateMedicamento)
+	r.Delete("/api/v1/farmacia/{id}", handlers.DeleteMedicamento)
+	fmt.Println("Servidor iniciado en http://localhost:8080")
+
+	http.ListenAndServe(":8080", r)
+}
