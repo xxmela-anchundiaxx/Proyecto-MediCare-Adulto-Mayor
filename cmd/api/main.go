c9d5d7a17ba53663fc9640cfbfb1165b6f606e4e
xxmela-anchundiaxx <melanieanchundia27@gmail.com>
2026-06-11 22:23:21 -0500

    Descripción de cada función
    Explicación

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -12,11 +12,15 @@ import (
 func main() {
 	// Crea una nueva instancia del router Chi
 	r := chi.NewRouter()
-
+    // Ruta para registrar un nuevo medicamento
 	r.Post("/api/v1/farmacia", handlers.CreateMedicamento)
+	// Ruta para obtener la lista de todos los medicamentos
 	r.Get("/api/v1/farmacia", handlers.GetMedicamentos)
+	// Ruta para obtener un medicamento específico mediante su ID
 	r.Get("/api/v1/farmacia/{id}", handlers.GetMedicamento)
+	// Ruta para actualizar la información de un medicamento por ID
 	r.Put("/api/v1/farmacia/{id}", handlers.UpdateMedicamento)
+	// Ruta para eliminar un medicamento por ID
 	r.Delete("/api/v1/farmacia/{id}", handlers.DeleteMedicamento)
 	fmt.Println("Servidor iniciado en http://localhost:8080")
 
